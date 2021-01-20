97e12fc76078eb7ef17b1b68f90746265969b886
Traka Dev <trakadev@gmail.com>
2021-01-20 02:45:49 -0600

    mensajes aleatorios

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const messages = [
+    "Jorge",
+    "Jessy",
+    "Alex",
+    "Manuel",
+    "Carolina"
+];
+
+const randomMsg = () => {
+    const message = messages[Math.floor(Math.random() * messages.length)];
+    console.log(message);
+};
+
+module.exports = { randomMsg };
